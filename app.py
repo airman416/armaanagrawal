76b1c324f767df2ce4cc8719a0452b21922dac8e
Armaan Agrawal <im.armaan.agrawal@gmail.com>
2024-08-31 15:23:21 -0400

    New main page

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
- 
+
+
+@app.route('/me')
+def me():
+    return render_template("me.html")
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=81)
